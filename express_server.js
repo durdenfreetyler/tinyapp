9f36716c454cca1118e416b837cb77386497911e
Abe <abrahamrodgers@gmail.com>
2023-01-25 13:12:58 -0800

    added username button and cookie implementation.

--- express_server.js
+++ express_server.js
@@ -95,3 +95,8 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
