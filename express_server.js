9d81fbbe7810d431b645b809f80914c2fc69ea24
Abe <abrahamrodgers@gmail.com>
2023-01-26 13:42:34 -0800

    FINALLY got the edge cases sorted out and removed the username funcionality out of my code.

--- express_server.js
+++ express_server.js
@@ -23,6 +23,14 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const users = {
+  userRandomId: {
+    id: "userRandomId",
+    email: "random@random.com",
+    passwords: 1234,
+  },
+};
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -53,23 +61,34 @@ app.get("/fetch", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
-  console.log(req.cookies["username"]);
-
+  console.log(`req.cookies["user_id"]`, req.cookies["user_id"]);
+  console.log("templateVars:", templateVars);
   res.render("urls_index", templateVars);
   // res.send(`Hello, I'm here for you.`);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const id = req.cookies.user_id;
+  const user = users[id];
+  const templateVars = { user };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+  const userId = req.cookies.user_id;
+  console.log("-----------------------");
+  console.log("users", users);
+  console.log("user_id", userId);
+  const user = users[userId];
+  console.log("user", user);
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
+    user: user,
   };
+  console.log("templateVars", templateVars);
   res.render("urls_show", templateVars);
 });
 
@@ -105,21 +124,65 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   // const templateVars = {
-  //   username: req.cookies["username"],
+  //   username: req.cookies["user_id"],
   // };
-
-  const username = req.body.username;
-  res.cookie("username", username);
+  const email =
+    // const username = req.body.username;
+    res.cookie("user_id");
   // res.render("urls_index", templateVars);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
-  console.log("hi there, stranger");
-  res.render("register");
+  const id = req.cookies.user_id;
+  const user = users[id];
+  const templateVars = { user };
+  res.render("register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  // const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const id = generateRandomString(6);
+
+  const newUser = {
+    id: id,
+    email: email,
+    passwords: password,
+  };
+
+  console.log("!!!!!!!", users);
+  users[id] = newUser;
+
+  console.log("users[id]", users[id]);
+  // console.log("users", users);
+  if (!email || !password) {
+    return res.status(400).send("please provide an email and a password");
+  }
+
+  let foundUser = null;
+  for (const userEmail in users) {
+    const user = userEmail[users];
+    console.log("users[id].email", users[id].email);
+    if (users[id].email === email) {
+      foundUser = user;
+      console.log("foundUser", foundUser);
+      return res
+        .status(400)
+        .send("there is already a user registered with that email");
+    }
+    console.log("foundUser:", foundUser);
+    if (!foundUser) {
+      return res.status(400).send("no user with that email found");
+    }
+  }
+  console.log("users[id].id", users[id].id);
+  res.cookie("user_id", users[id].id);
+  res.redirect("/urls");
 });
